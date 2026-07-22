0b626e6b84f37ab3aa11303958d4c5ef896965de
artko <artkor05@gmail.com>
2026-07-22 11:30:16 +0500

    day 18 end

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -7,3 +7,10 @@ class Women(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
+
+class AgainWomen(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
