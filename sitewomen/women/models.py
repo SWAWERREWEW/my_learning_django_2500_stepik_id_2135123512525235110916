2ff5ba7c9f1cdb2c9987aa4d9f8fe6e9c59a49a1
artko <artkor05@gmail.com>
2026-07-24 13:39:36 +0500

    day 23 end and add migrations from 22 day

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -2,26 +2,38 @@
 from django.db import models
 from django.urls import reverse
 
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_published=Women.Status.PUBLISHED)
+
+
 # Create your models here.
 class Women(models.Model):
-    title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=300, unique=True, db_index=True)
-    content = models.TextField(blank=True)
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
-
     class Meta:
         ordering = ['time_create']
         indexes = [
             models.Index(fields=['time_create'])
         ]
 
+    class Status(models.IntegerChoices):
+        DRAFT = 0, 'Черновик'
+        PUBLISHED = 1, 'Опубликовано'
+
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_slug': self.slug})
+
+    objects = models.Manager()
+    published = PublishedManager()
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=300, unique=True, db_index=True)
+    content = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
 
 class AgainWomen(models.Model):
     title = models.CharField(max_length=255)
