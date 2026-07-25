a037bbf26a4333fd623c174454b193546c95bcf5
artko <artkor05@gmail.com>
2026-07-25 12:20:57 +0500

    day 25 end

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -34,6 +34,7 @@ class Women(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT)
 
 class AgainWomen(models.Model):
     title = models.CharField(max_length=255)
@@ -41,3 +42,10 @@ class AgainWomen(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
