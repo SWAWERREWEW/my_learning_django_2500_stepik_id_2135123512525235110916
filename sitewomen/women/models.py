f1c8c3a511791e70c893369241e772967296ef4d
artko <artkor05@gmail.com>
2026-07-26 16:07:26 +0500

    day 28 end and add pictures for day 27

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -35,17 +35,27 @@ class Women(models.Model):
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
     cat = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='posts')
+    tags = models.ManyToManyField('TagPost', blank=True, related_name='tags')
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True)
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
+    # tags = models.ManyToManyField('Women', blank=True, related_name='Womans')
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_slug': self.slug})
+
+
+class TagPost(models.Model):
+    tag = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=150, unique=True)
+
+    def __str__(self):
+        return self.tag
 
 
 class AgainWomen(models.Model):
