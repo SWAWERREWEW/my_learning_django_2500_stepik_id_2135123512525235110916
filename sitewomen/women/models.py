a138d4d481a127e5a46c8ad6daa877481b89cb01
artko <artkor05@gmail.com>
2026-07-28 05:36:22 +0500

    day 30 end

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -36,6 +36,8 @@ class Women(models.Model):
     is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
     cat = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='posts')
     tags = models.ManyToManyField('TagPost', blank=True, related_name='tags')
+    husband = models.OneToOneField('Husband', on_delete=models.SET_NULL, null=True, blank=True,
+        related_name='wuman')
 
 
 class Category(models.Model):
@@ -59,6 +61,14 @@ class TagPost(models.Model):
 
     def get_absolute_url(self):
         return reverse('tag', kwargs={'tag_slug': self.slug})
+
+
+class Husband(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class AgainWomen(models.Model):
