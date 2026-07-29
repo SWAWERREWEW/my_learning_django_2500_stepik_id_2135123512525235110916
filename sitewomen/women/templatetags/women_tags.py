48314d415bac638f7385ee8e318ab252d0c47c9c
artko <artkor05@gmail.com>
2026-07-29 08:56:24 +0500

    day 35 end

--- sitewomen/women/templatetags/women_tags.py
+++ sitewomen/women/templatetags/women_tags.py
@@ -2,6 +2,7 @@
 from django import template
 from women.views import cats_db
 from women.models import Category, TagPost
+from django.db.models import Count
 
 # Регистрация новых тегов
 register = template.Library()
@@ -13,10 +14,10 @@ def get_categories():
 
 @register.inclusion_tag('women/list_categories.html')
 def show_categories(cat_selected=0):
-    cats = Category.objects.all()
+    cats = Category.objects.annotate(total=Count("posts")).filter(total__gt=0)
     return {'cats': cats, 'cat_selected': cat_selected}
 
 
 @register.inclusion_tag('women/list_tags.html')
 def show_all_tags():
-    return {'tags': TagPost.objects.all()}
+    return {'tags': TagPost.objects.annotate(total=Count("tags")).filter(total__gt=0)}
