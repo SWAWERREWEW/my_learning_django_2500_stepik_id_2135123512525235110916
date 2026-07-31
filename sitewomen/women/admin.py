442a067052ddf37d53b31903d5d1f1c1b63d3dd8
artko <artkor05@gmail.com>
2026-07-31 10:38:21 +0500

    day 38 end and edit .gitignore

--- sitewomen/women/admin.py
+++ sitewomen/women/admin.py
@@ -1,6 +1,20 @@
 # sitewomen\women\admin.py
 from django.contrib import admin
-from .models import Women
+from .models import Women, Category
 
-# Register your models here.
-admin.site.register(Women)
+
+@admin.register(Women)
+class WomenAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "time_create", "is_published", "cat")
+    list_display_links = ("id", "time_create")
+    ordering = ["time_create"]
+    list_editable = ("is_published", "title", "cat")
+    # list_per_page = 1
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "slug")
+    list_display_links = ("id", "name", "slug")
+
+# Register your models here. 
+# admin.site.register(Women, WomenAdmin)
--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -29,23 +29,28 @@ class Women(models.Model):
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_slug': self.slug})
 
+
     objects = models.Manager()
     published = PublishedManager()
-    title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=300, unique=True, db_index=True)
-    content = models.TextField(blank=True)
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='posts')
-    tags = models.ManyToManyField('TagPost', blank=True, related_name='tags')
+    title = models.CharField(max_length=255, verbose_name="зугаловах")
+    slug = models.SlugField(max_length=300, unique=True, db_index=True, verbose_name="СЛагъ")
+    content = models.TextField(blank=True, verbose_name="КАНТЕНТсодержимое")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Тайм рождения")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Обнова")
+    is_published = models.BooleanField(choices=tuple(map(lambda x: (bool(x[0]), x[1]), Status.choices)),
+        default=Status.DRAFT, verbose_name="Стутус")
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='posts', verbose_name="Котигория")
+    tags = models.ManyToManyField('TagPost', blank=True, related_name='tags', verbose_name="Тегишки")
     husband = models.OneToOneField('Husband', on_delete=models.SET_NULL, null=True, blank=True,
-        related_name='wuman')
-
+        related_name='wuman', verbose_name="Мужьяки")
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
-    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+    class Meta:
+        verbose_name = "Категорька из категорий в классе Мета"
+        verbose_name_plural = "Категорьки из категорий в классе Мета"
+
+    name = models.CharField(max_length=100, db_index=True, verbose_name="ИменаНоНеймов")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="СЛагъ")
     # tags = models.ManyToManyField('Women', blank=True, related_name='Womans')
 
     def __str__(self):
