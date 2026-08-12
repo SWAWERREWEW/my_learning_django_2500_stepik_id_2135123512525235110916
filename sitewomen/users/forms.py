e66dca63ca1daa5895da17168e276141b7047d75
artko <artkor05@gmail.com>
2026-08-12 10:19:48 +0500

    day 62 end

--- sitewomen/users/forms.py
+++ sitewomen/users/forms.py
@@ -1,6 +1,13 @@
 # sitewomen/users/forms.py
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import get_user_model
 
-class LoginUserForm(forms.Form):
+
+class LoginUserForm(AuthenticationForm):
     username = forms.CharField(label="🏠Погин", widget=forms.TextInput(attrs={"class": "form-input"}))
     password = forms.CharField(label="🗝Ларопь", widget=forms.PasswordInput(attrs={"class": "form-input"}))
+
+    class Meta:
+        model = get_user_model()
+        fields = ["username", "password"]
--- sitewomen/users/urls.py
+++ sitewomen/users/urls.py
@@ -1,5 +1,6 @@
 ﻿# sitewomen\users\urls.py
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from . import views
 
 app_name = "users"
@@ -8,6 +9,6 @@ app_name = "users"
 urlpatterns = [
     # users:
     # users:login
-    path('login/', views.login_user, name='login'),
-    path('logout/', views.logout_user, name='logout'),
+    path('login/', views.LoginUser.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
