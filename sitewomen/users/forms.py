dfef1c1485fcd09e9fbfafd28fe2dc0bec66fb09
artko <artkor05@gmail.com>
2026-08-13 10:58:10 +0500

    day 65 end

--- sitewomen/users/forms.py
+++ sitewomen/users/forms.py
@@ -1,6 +1,6 @@
 # sitewomen/users/forms.py
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import get_user_model
 
 
@@ -12,26 +12,24 @@ class LoginUserForm(AuthenticationForm):
         model = get_user_model()
         fields = ["username", "password"]
 
-class RegisterUserForm(forms.ModelForm):
-    username = forms.CharField(label="🏠Погин")
-    password = forms.CharField(label="🗝Ларопь", widget=forms.PasswordInput())
-    password2 = forms.CharField(label="🗝Снова Ларопь", widget=forms.PasswordInput())
+class RegisterUserForm(UserCreationForm):
+    username = forms.CharField(label="🏠Погин", widget=forms.TextInput(attrs={"class": "form-input"}))
+    password1 = forms.CharField(label="🗝Ларопь", widget=forms.PasswordInput(attrs={"class": "form-input"}))
+    password2 = forms.CharField(label="🗝Снова Ларопь", widget=forms.PasswordInput(attrs={"class": "form-input"}))
 
     class Meta:
         model = get_user_model()
-        fields = ["username", "email", "first_name", "last_name", "password", "password2"]
+        fields = ["username", "email", "first_name", "last_name", "password1", "password2"]
         labels = {
             "email": "📦Почта",
             "first_name": "📐Имечко",
             "last_name": "🍀Фамилия"
         }
-
-    # Проверка паролей
-    def clean_password2(self):
-        cd = self.cleaned_data
-        if cd["password"] != cd["password2"]:
-            raise forms.ValidationError("❌⚠🗝Пароли без совпадения🗝⚠❌")
-        return cd["password"]
+        widgets = {
+            "email": forms.TextInput(attrs={"class": "form-input"}),
+            "first_name": forms.TextInput(attrs={"class": "form-input"}),
+            "last_name": forms.TextInput(attrs={"class": "form-input"})
+        }
 
     # Проверка уникальности передаваемых почт
     def clean_email(self):
--- sitewomen/users/views.py
+++ sitewomen/users/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 from django.urls import reverse
 
 from django.contrib.auth.views import LoginView
+from django.urls.base import reverse_lazy
+from django.views.generic import CreateView
 
 from .forms import LoginUserForm, RegisterUserForm
 
@@ -15,14 +17,8 @@ class LoginUser(LoginView):
     template_name = "users/login.html"
     extra_context = {"title": "авторизация"}
 
-def register(request):
-    if request.method == "POST":
-        form = RegisterUserForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.set_password(form.cleaned_data['password'])
-            user.save()
-            return render(request, 'users/register_done.html')
-    else:
-        form = RegisterUserForm()
-    return render(request, 'users/register.html', {'form': form})
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = "users/register.html"
+    extra_context = {"title": "Регистрейшон🎂🎂🎂"}
+    success_url = reverse_lazy('users:login')
